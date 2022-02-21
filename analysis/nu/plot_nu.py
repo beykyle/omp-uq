c972231cba9dfe5355c5a67b240e4fa7c2e94a89
beykyle <beykyle@umich.edu>
2022-02-21 15:05:28 -0500

    Initial commit of nubar deviation plotting

--- analysis/nu/plot_nu.py
+++ analysis/nu/plot_nu.py
@@ -17,20 +17,37 @@ mpl.rcParams['xtick.major.pad'] = '10'
 mpl.rcParams['ytick.major.pad'] = '10'
 mpl.rcParams['image.cmap'] = 'BuPu'
 
-data_dir  = "/home/beykyle/umich/omp-uq/analysis/pfns/data"
+data_dir  = "/home/beykyle/umich/omp-uq/analysis/nu/data"
 workdir   = './'
 histFile  = 'histories.out'
 timeFile  = 'histories.out'
 yeildFile = 'yeilds.cgmf.0'
 nevents = int(1E4)
 
-nu  = []
-pnu = []
+nu = []
 
 
-for filename in os.scandir(data_dir):
-    if filename.is_file():
-        n = np.load(filename.path)
-        p = np.load(filename.path)
-        nubar = np.dot(n,p)
-        print(nubar)
+for i in [11,13,15,25,26,30,31,40,52,54,55,60,61,63,71,75,82,84,88,94]:
+    nu_fname  = data_dir +  "/nu_" + str(i) + ".npy"
+    pnu_fname = data_dir + "/pnu_" + str(i) + ".npy"
+    n = np.load(nu_fname)
+    p = np.load(pnu_fname)
+    plt.semilogy(n,p)
+    nubar = np.dot(n,p)
+    nu.append(nubar)
+
+plt.xlabel(r"$\nu$")
+plt.ylabel(r"$P(\nu)$")
+plt.show()
+#plt.savefig("pnu.pdf")
+
+
+plt.hist(100*(np.array(nu)-3.7676)/3.7676,bins=6)
+plt.xlabel(r"Percent relative deviation $\bar{\nu}$")
+plt.ylabel(r"frequency")
+plt.tight_layout()
+plt.savefig("pnubar.pdf")
+
+
+print(np.mean(nu))
+print(np.sqrt(np.var(nu)))
