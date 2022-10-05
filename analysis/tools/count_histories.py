29b4a8b055097b0af5ccc184e3cbe58cda452b65
Kyle Andrew Beyer <beykyle@umich.edu>
2022-10-05 16:28:26 -0400

    print fission histories, not fragment histories

--- analysis/tools/count_histories.py
+++ analysis/tools/count_histories.py
@@ -3,4 +3,4 @@ from CGMFtk import histories as fh
 
 if __name__ == "__main__":
     hist = fh.Histories(sys.argv[1])
-    print(len(hist.getFissionHistories()))
+    print(int(len(hist.getFissionHistories())/2))
