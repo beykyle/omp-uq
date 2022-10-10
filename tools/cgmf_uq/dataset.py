4c927f59f0881caa50c05b4af6eb3e1b5a08c525
beykyle <beykyle@umich.edu>
2022-10-10 12:37:35 -0600

    pull in plotting updates

--- tools/cgmf_uq/dataset.py
+++ tools/cgmf_uq/dataset.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 class DataSetUQUncorr:
     def __init__(self, results_dir: Path, num_samples: int,
-            num_hist: int, label : str, default_sample_name="", corr=False):
+            num_hist: int, label : str, default_sample_name="",
+            corr=False, min_sample=0, is_def=False):
 
         self.label = label
         self.num_ebins   = 99
@@ -12,7 +13,7 @@ class DataSetUQUncorr:
         self.num_hist    = num_hist
 
         # initialize sample data
-        self.nubar = np.load(str(results_dir / "nubars.npy"))
+        self.nubar = np.load(str(results_dir / "nubars.npy"))[min_sample:min_sample+num_samples]
         self.ebins = np.zeros((num_samples,self.num_ebins))
         self.pfns  = np.zeros((num_samples,self.num_ebins))
         self.nu    = np.zeros((num_samples,self.num_nu_bins))
@@ -26,28 +27,41 @@ class DataSetUQUncorr:
         self.pnu_default   = np.zeros(self.num_nu_bins)
 
         # read in samples
-        for i in range(0, num_samples):
+        for i in range(min_sample, min_sample + num_samples):
             sample_name = str(i) + ".npy"
             nu_tmp  = np.load(str(results_dir / ("nu_"     + sample_name)))
             pnu_tmp = np.load(str(results_dir / ("pnu_"    + sample_name)))
-            self.nu[i,0:nu_tmp.shape[0]]   = nu_tmp
-            self.pnu[i,0:pnu_tmp.shape[0]] = pnu_tmp
-            self.ebins[i,:] = np.load(str(results_dir / ("ebins_"  + sample_name)))
-            self.pfns[i,:]  = np.load(str(results_dir / ("pfns_"   + sample_name)))
+            idx = i - min_sample
+            self.nu[idx,0:nu_tmp.shape[0]]   = nu_tmp
+            self.pnu[idx,0:pnu_tmp.shape[0]] = pnu_tmp
+            self.ebins[idx,:] = np.load(str(results_dir / ("ebins_"  + sample_name)))
+            self.pfns[idx,:]  = np.load(str(results_dir / ("pfns_"   + sample_name)))
 
-        # read in defaults
-        default_dir = results_dir / "default"
-        nu_tmp  = np.load(str(default_dir / ("nu"  + default_sample_name + ".npy")))
-        pnu_tmp = np.load(str(default_dir / ("pnu" + default_sample_name + ".npy")))
-        self.nu_default[0:nu_tmp.shape[0]]   = nu_tmp
-        self.pnu_default[0:pnu_tmp.shape[0]] = pnu_tmp
-        self.ebins_default = np.load(str(default_dir / ("ebins"  + default_sample_name + ".npy")))
-        self.pfns_default  = np.load(str(default_dir / ("pfns"   + default_sample_name + ".npy" )))
-        self.nubar_default = np.load(str(default_dir / ("nubars" + default_sample_name + ".npy")))
+        # handle defaults
+        if is_def:
+            # default is just statistical mean
+            self.nubar_default = np.mean(self.nubar)
+            self.ebins_default = self.ebins
+            self.pfns_default  = np.mean(self.pfns,axis=0)
+            self.nu_default    = self.nu
+            self.pnu_default   = np.mean(self.pnu,axis=0)
+        else:
+            # read in seperate default sample
+            default_dir = results_dir / "default"
+            nu_tmp  = np.load(str(default_dir / ("nu"  + default_sample_name + ".npy")))
+            pnu_tmp = np.load(str(default_dir / ("pnu" + default_sample_name + ".npy")))
+            self.nu_default[0:nu_tmp.shape[0]]   = nu_tmp
+            self.pnu_default[0:pnu_tmp.shape[0]] = pnu_tmp
+            self.ebins_default = np.load(
+                    str(default_dir / ("ebins"  + default_sample_name + ".npy")))
+            self.pfns_default  = np.load(
+                    str(default_dir / ("pfns"   + default_sample_name + ".npy" )))
+            self.nubar_default = np.load(
+                    str(default_dir / ("nubars" + default_sample_name + ".npy")))[0]
 
         if corr:
-            self.event_nu_n = np.zeros((254, self.num_hist))
-            self.event_nu_p = np.zeros((254, self.num_hist))
+            self.event_nu_n = np.zeros((num_samples, self.num_hist))
+            self.event_nu_p = np.zeros((num_samples, self.num_hist))
 
             self.event_nu_n_default = np.load(
                         str(default_dir / str("event_nu_n" + default_sample_name + ".npy")),
@@ -58,14 +72,14 @@ class DataSetUQUncorr:
                         allow_pickle=True
                     )
 
-            #for i in range(0, num_samples):
-            for i in range(0,253):
+            for i in range(min_sample, min_sample + num_samples):
                 sample_name = str(i)
-                self.event_nu_n[i,:] = np.load(
+                idx = i - min_sample
+                self.event_nu_n[idx,:] = np.load(
                         str(results_dir / str("event_nu_n_" + sample_name + ".npy")),
                         allow_pickle=True
-                    )
-                self.event_nu_p[i,:] = np.load(
+                        )[:num_hist]
+                self.event_nu_p[idx,:] = np.load(
                         str(results_dir / str("event_nu_p_" + sample_name + ".npy")),
                         allow_pickle=True
-                    )
+                        )[:num_hist]
