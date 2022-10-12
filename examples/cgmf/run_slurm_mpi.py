f6ecdd998e7e380bfe65588951496af7cfc2eda9
Kyle Andrew Beyer <beykyle@umich.edu>
2022-10-12 17:39:17 -0400

    update run and post scripts to handle default cases

--- examples/cgmf/run_slurm_mpi.py
+++ examples/cgmf/run_slurm_mpi.py
@@ -6,14 +6,14 @@ from pathlib import Path
 from cgmf_uq import run_ensemble
 
 # where are we putting the results
-res_dir = Path("/home/beykyle/turbo/omp_uq/KDUQ")
+res_dir = Path("/home/beykyle/turbo/omp_uq/KDUQ/u235/histories_192E3")
 
 # optical model param file - use default params
 omp_fpath = Path("/home/beykyle/OM/KDOMPuq/s")
 
-# 252Cf sf
-target_zaid = 98252
-e_inc = 0.0
+# U235 (n,th)
+target_zaid = 92235
+e_inc_MeV = 25.0E-9
 
 # run num_hist total histories, dividing evenly across SLURM resources
 num_hist = int(192E3)
@@ -24,6 +24,12 @@ num_hist_per_cpu = int(num_hist / (nodes * cpus_per_node))
 print("Running {} histories, on {} nodes and {} tasks per node, for {} histories / cpu"
         .format(num_hist, nodes, cpus_per_node, num_hist_per_cpu))
 
+# run default
+default_dir = res_dir / "default"
+default_omp_file = Path("/home/beykyle/OM/KDOMPuq/KDGlobal.json")
+run_ensemble(default_omp_file, default_dir, num_hist_per_cpu, target_zaid, e_inc_MeV, "default", slurm=False)
+
+# run UQ
 for i in range(0,416):
     omp_file = omp_fpath / (str(i) + ".json")
-    run_ensemble(omp_file, res_dir, num_hist_per_cpu, target_zaid, e_inc, str(i), slurm=False)
+    run_ensemble(omp_file, res_dir, num_hist_per_cpu, target_zaid, e_inc_MeV, str(i), slurm=False)
