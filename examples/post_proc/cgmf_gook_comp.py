2d7bfd2b2c7d1b15e808288821041c73905294d5
beykyle <beykyle@umich.edu>
2022-10-23 13:59:24 -0600

    Add proper mean shift

--- examples/post_proc/cgmf_gook_comp.py
+++ examples/post_proc/cgmf_gook_comp.py
@@ -54,47 +54,127 @@ def lh3d():
     ax_l.get_yaxis().set_major_locator(MaxNLocator(4,integer=True))
     plt.show()
 
+def mass_dep_plots(cf252_A_pfns_cgmf, cf252_A_pfns_gook, masses, masses_odd, masses_even):
+    ebins = np.arange(0.4, 5., step=0.2)
+
+    specs_cgmf = cf252_A_pfns_cgmf.getSpecs(masses)
+    specs_gook = cf252_A_pfns_gook.getSpecs(masses)
 
 
-cf252_A_pfns_gook = PFNS_A("/home/beykyle/umich/omp-uq/data/exfor/252_Cf_PFNS_A.npy")
-cf252_A_pfns_cgmf  = PFNS_A("/home/beykyle/db/aps/gook/cgmf_252cf_kddef_pfns_a.npy")
-
-ebins = np.arange(0.4, 5., step=0.4)
-masses = np.array(range(92, 160), dtype=int)
-masses_odd  = np.array(range(92, 160,2), dtype=int)
-masses_even = np.array(range(93, 161,2), dtype=int)
-
-specs_cgmf = cf252_A_pfns_cgmf.getSpecs(masses)
-specs_gook = cf252_A_pfns_gook.getSpecs(masses)
-
-
-x,y,z, ax = plotSpecRatioColor(
-        specs_cgmf, specs_gook, ebins, "CGMF", "GOOK", masses, r"$A $[u]", rel_diff=True)
+    x,y,z, ax = plotSpecRatioColor(
+            specs_cgmf, specs_gook, ebins, "CGMF", "GOOK", masses, r"$A $[u]", rel_diff=True)
 
 #ax.set_zlim3d(-200,300)
-ax.get_yaxis().set_major_locator(MaxNLocator(4,integer=True))
+    ax.get_yaxis().set_major_locator(MaxNLocator(4,integer=True))
 #ax.plot_surface(x,y, np.zeros(z.shape), alpha=0.3, color='k')
-plt.savefig("PFNS_A_cgmf_vs_gook.pdf")
-plt.close()
+    plt.savefig("PFNS_A_cgmf_vs_gook.pdf")
+    plt.close()
 
-# TODO add error bars
-mean_ratio = np.zeros(masses.shape)
-for i , cgmf in enumerate(specs_cgmf):
-    gook = specs_gook[i]
-    me = hardness(cgmf,gook, ebins, rel=True)
-    mean_ratio[i] = me
+    mean_ratio = np.zeros(masses.shape)
 
-mean_ratio_even = mean_ratio[ np.where( masses % 2 ==0 )]
-mean_ratio_odd = mean_ratio[ np.where( masses % 2 != 0 )]
+    for i , cgmf in enumerate(specs_cgmf):
+        gook = specs_gook[i]
+        me = hardness(cgmf,gook, ebins, rel=True)
+        mean_ratio[i] = me
 
-plt.xlabel(r"$A$ [u]")
-plt.ylabel(r"$\frac{\bar{E}_{CGMF} - \bar{E}_{GOOK}}{\bar{E}_{GOOK}}$ [%]")
-plt.plot(masses_even, mean_ratio_even, label=r"$A$ even")
-plt.plot(masses_odd, mean_ratio_odd, label=r"$A$ odd")
-plt.legend()
-plt.tight_layout()
-#plt.show()
-plt.savefig("mean_E_A_cgmf_vs_gook.pdf")
-plt.close()
+    mean_ratio_even = mean_ratio[ np.where( masses % 2 ==0 )]
+    mean_ratio_odd = mean_ratio[ np.where( masses % 2 != 0 )]
 
-interesting_masses(cf252_A_pfns_cgmf, cf252_A_pfns_gook, ebins)
+    plt.xlabel(r"$A$ [u]")
+    plt.ylabel(r"$\frac{\bar{E}_{CGMF} - \bar{E}_{GOOK}}{\bar{E}_{GOOK}}$ [%]")
+    plt.plot(masses_even, mean_ratio_even, label=r"$A$ even")
+    plt.plot(masses_odd, mean_ratio_odd, label=r"$A$ odd")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("mean_E_A_cgmf_vs_gook.pdf")
+    plt.close()
+
+    interesting_masses(cf252_A_pfns_cgmf, cf252_A_pfns_gook, ebins)
+
+def getMeanShiftErr(masses, pfns, pfns_ref, label, label_ref):
+    ebins = np.arange(0.3, 6., step=0.2)
+
+    # common interpolate and normalize
+    specs  = [ (s.interp(ebins)).normalize() for s in  pfns.getSpecs(masses)     ]
+    specsr = [ (s.interp(ebins)).normalize() for s in  pfns_ref.getSpecs(masses) ]
+
+
+    mean   = np.array([ s.mean() for s in  specs  ])
+    meanr  = np.array([ s.mean() for s in  specsr ])
+
+    var    = np.array([ np.sqrt(s.variance()) for s in  specs  ])
+    varr   = np.array([ np.sqrt(s.variance()) for s in  specsr ])
+
+    dmean  = np.array([ np.sqrt(s.var_in_mean()) for s in  specs])
+    dmeanr = np.array([ np.sqrt(s.var_in_mean()) for s in  specsr ])
+
+    fig = plt.figure()
+
+    #plt.errorbar(masses, mean , yerr=dmean, label=label)
+    #plt.errorbar(masses, meanr, yerr=dmeanr, label=label_ref)
+
+    p1 = plt.plot(masses, mean, label=label)
+    #plt.fill_between( masses, mean - dmean/2, mean + dmean/2, color=p1[0].get_color(), alpha=0.4)
+    p2 = plt.plot(masses, meanr, label=label_ref)
+    #plt.fill_between( masses, meanr - dmeanr/2, meanr + dmeanr/2, color=p2[0].get_color(), alpha=0.4)
+
+    plt.fill_between( masses, mean - var/2, mean + var/2, color=p1[0].get_color(), alpha=0.4)
+    plt.fill_between( masses, meanr - varr/2, meanr + varr/2, color=p2[0].get_color(), alpha=0.4)
+
+    plt.legend()
+    plt.xlabel(r"$A$ [u]")
+    plt.ylabel(r"$\langle E \rangle$ [MeV]")
+    plt.tight_layout()
+
+    return fig, mean, meanr, dmean, dmeanr
+
+def getHardnessAboveMaxwell(masses, pfns):
+    ebins = np.arange(0.4, 5., step=0.2)
+
+    # common interpolate and normalize
+    specs  = [ s.interp(ebins) for s in  pfns.getSpecs(masses)]
+
+    #TODO
+
+
+
+def plotIndividual(masses,cgmf, gooK):
+    datasets = [cf252_A_pfns_cgmf_kd, cf252_A_pfns_gook]
+    labels   = ["CGMF+KD", "Gook"]
+
+    ratio=True
+    if not ratio:
+        for A in masses:
+            fig, _, _, _ = plotCompPFNS(A, datasets, labels, maxwell_norm=False)
+            plt.xlim([0,5])
+            plt.ylim([0.5*1E-2, 1E0])
+            #plt.ylim([0.5*1E-1, 0.21*1E1])
+            #plt.yscale("linear")
+            plt.tight_layout()
+            plt.show()
+
+    else:
+        for A in masses:
+            fig, _, _, _ = plotCompPFNS(A, datasets, labels, maxwell_norm=True)
+            plt.xlim([0,5])
+            plt.ylim([0.5*1E-1, 0.21*1E1])
+            plt.yscale("linear")
+            plt.tight_layout()
+            plt.show()
+
+
+if __name__ == "__main__":
+    # read in spectra
+    cf252_A_pfns_gook = PFNS_A("/home/beykyle/umich/omp-uq/data/exfor/252_Cf_PFNS_A.npy")
+    cf252_A_pfns_cgmf_kd  = PFNS_A("./cgmf_252cf_kddef_pfns_a.npy")
+
+    masses = np.array(range(85, 165), dtype=int)
+    mask = np.array( [ cf252_A_pfns_cgmf_kd.getSpecs([A])[0].norm() > 1000 for A in masses ] )
+    masses = masses[np.where(mask)]
+    masses_odd  = np.array(range(86, 164,2), dtype=int)
+    masses_even = np.array(range(85, 165,2), dtype=int)
+
+    fig, _, _, _, _, = getMeanShiftErr(
+            masses, cf252_A_pfns_cgmf_kd, cf252_A_pfns_gook, "CGMF+KD", "GOOK")
+    plt.show()
+
