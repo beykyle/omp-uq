c1f5c35c17cbede2f2be6909608223d708a3421c
Kyle Andrew Beyer <beykyle@umich.edu>
2023-03-21 09:35:04 -0400

    add quantity choice to HistData

--- examples/post_proc/run_post_proc.py
+++ examples/post_proc/run_post_proc.py
@@ -59,9 +59,14 @@ def hist_from_list_of_lists(num, lol, bins, mask_generator=None, out=False):
     return hist, m
 
 class HistData:
-    def __init__(self, ensemble_range):
-        # TODO pass in desired quantities
+    def __init__(self, ensemble_range : tuple, quantities : list):
         #TODO mc uncertanties
+
+        self.quantities = quantities
+
+        self.scalar_qs = {}
+        self.vector_qs = {}
+        self.tensor_qs = {}
 
         # get ensemble range
         self.min_ensemble = ensemble_range[0]
@@ -97,32 +102,50 @@ class HistData:
         self.pfnsA  = np.zeros((nensemble, self.abins.size, self.ecenters.size))
         self.pfgsA  = np.zeros((nensemble, self.abins.size, self.ecenters.size))
 
-        self.nuTKEA = np.zeros((nensemble, self.TKEbins.size, self.abins.size))
+        self.nuATKE = np.zeros((nensemble, self.TKEbins.size, self.abins.size))
 
-        self.scalar_qs = { "nubar" : np.zeros((nensemble)), "nugbar" : np.zeros((nensemble)) }
-
-        self.vector_qs = {
-                "nubarA"     : np.zeros((nensemble, self.abins.size))      ,
-                "nugbarA"    : np.zeros((nensemble, self.abins.size))      ,
-                "nubarZ"     : np.zeros((nensemble, self.zbins.size))      ,
-                "nugbarZ"    : np.zeros((nensemble, self.zbins.size))      ,
-                "nubarTKE"   : np.zeros((nensemble, self.TKEcenters.size)) ,
-                "nugbarTKE"  : np.zeros((nensemble, self.TKEcenters.size)) ,
-                "pnu"        : np.zeros((nensemble, self.nubins.size))     ,
-                "pnug"       : np.zeros((nensemble, self.nugbins.size))    ,
-                "egtbarnu"   : np.zeros((nensemble, self.nubins.size))     ,
-                "pfns"       : np.zeros((nensemble, self.ecenters.size))   ,
-                "pfgs"       : np.zeros((nensemble, self.ecenters.size))   ,
-                "multratioA" : np.zeros((nensemble, self.abins.size))
-                }
-
-        self.tensor_qs = {
-                "pfnsTKE" : np.zeros((nensemble, self.TKEcenters.size, self.ecenters.size))  ,
-                "pfgsTKE" : np.zeros((nensemble, self.TKEcenters.size, self.ecenters.size))  ,
-                "pfnsA"   : np.zeros((nensemble, self.abins.size, self.ecenters.size))  ,
-                "pfgsA"   : np.zeros((nensemble, self.abins.size, self.ecenters.size))  ,
-                "nuTKEA"  : np.zeros((nensemble, self.TKEcenters.size, self.abins.size))
-                }
+        for q in quantities:
+            if q == "nubar":
+                self.scalar_qs["nubar"] = np.zeros((nensemble))
+            elif q == "nugbar":
+                self.scalar_qs["nugbar"] = np.zeros((nensemble))
+            elif q == "nubarA":
+                self.scalar_qs["nubarA"] = np.zeros((nensemble, self.abins.size))
+            elif q == "nugbarA":
+                self.scalar_qs["nugbarA"] = np.zeros((nensemble, self.abins.size))
+            elif q == "nubarZ":
+                self.scalar_qs["nubarZ"] = np.zeros((nensemble, self.zbins.size))
+            elif q == "nugbarZ":
+                self.scalar_qs["nugbarZ"] = np.zeros((nensemble, self.zbins.size))
+            elif q == "nubarTKE":
+                self.scalar_qs["nubarTKE"] = np.zeros((nensemble, self.TKEcenters.size))
+            elif q == "nugbarTKE":
+                self.scalar_qs["nugbarTKE"] = np.zeros((nensemble, self.TKEcenters.size))
+            elif q == "pnu" :
+                self.scalar_qs["pnu" ] = np.zeros((nensemble, self.nubins.size))
+            elif q == "pnug":
+                self.scalar_qs["pnug"] = np.zeros((nensemble, self.nugbins.size))
+            elif q == "egtbarnu":
+                self.scalar_qs["egtbarnu"] = np.zeros((nensemble, self.nubins.size))
+            elif q == "pfns":
+                self.scalar_qs["pfns"] = np.zeros((nensemble, self.ecenters.size))
+            elif q == "pfgs":
+                self.scalar_qs["pfgs"] = np.zeros((nensemble, self.ecenters.size))
+            elif q == "multratioA":
+                self.scalar_qs["multratioA"] = np.zeros((nensemble, self.abins.size))
+            elif q == "pfnsTKE":
+                self.scalar_qs["pfnsTKE"] = np.zeros((nensemble, self.TKEcenters.size, self.ecenters.size))
+            elif q == "pfgsTKE":
+                self.scalar_qs["pfgsTKE"] = np.zeros((nensemble, self.TKEcenters.size, self.ecenters.size))
+            elif q == "pfnsA":
+                self.scalar_qs["pfnsA"] = np.zeros((nensemble, self.abins.size, self.ecenters.size))
+            elif q == "pfgsA":
+                self.scalar_qs["pfgsA"] = np.zeros((nensemble, self.abins.size, self.ecenters.size))
+            elif q == "nuATKE":
+                self.scalar_qs["nuATKE"] = np.zeros((nensemble, self.abins.size, self.TKEcenters.size))
+            else:
+                print("Unkown quantity: {}".format(q))
+                exit(1)
 
     def concat_from_array_job(num_jobs):
         interval = int(total_ensembles / num_jobs)
@@ -159,31 +182,41 @@ class HistData:
     def process_ensemble(hs : CGMFtk.Histories, n : int):
 
         # scalars
-        scalar_qs[ "nubar"][n]  = hs.nubar()
-        scalar_qs["nugbar"][n] = hs.nubarg(timeWindow=None, Eth=Ethg)
+        if "nubar" is in scalar_qs:
+            scalar_qs[ "nubar"][n] = hs.nubar()
+        if "nugbar" is in scalar_qs:
+            scalar_qs["nugbar"][n] = hs.nubarg(timeWindow=None, Eth=Ethg)
 
         # multiplicity dependent vector quantities
-        vector_qs[ "pnu"][n] = hs.Pnu(Eth=self.Ethn)
-        vector_qs["pnug"][n] = hs.Pnug(Eth=self.Ethg)
+        if "pnu" is in vector_qs:
+            vector_qs[ "pnu"][n] = hs.Pnu(Eth=self.Ethn)
+        if "pnug" is in vector_qs:
+            vector_qs["pnug"][n] = hs.Pnug(Eth=self.Ethg)
 
         # energy dependent vector quantities
-        vector_qs["pfns"][n]  = hs.pfns(egrid=self.ebins, Eth=self.Ethn)
-        vector_qs["pfgs"][n]  = hs.pfgs(egrid=self.ebins, Eth=self.Ethg,
-                                        minTime=self.min_time, maxTime=self.max_time)
+        if "pfns" is in vector_qs:
+            vector_qs["pfns"][n]  = hs.pfns(egrid=self.ebins, Eth=self.Ethn)
+        if "pfgs" is in vector_qs:
+            vector_qs["pfgs"][n]  = hs.pfgs(
+                    egrid=self.ebins, Eth=self.Ethg,
+                    minTime=self.min_time, maxTime=self.max_time)
 
         # nu dependent
-        for l, nu in enumerate(self.nubins):
-            mask        = np.where(hs.getNu() == nu)
-            num_gammas  = np.sum( hs.getNug() [mask] )
-            nglab       = hs.getGamElab() [mask]
-            _ , vector_qs["egtbarnu"][n,l] = hist_from_list_of_lists(
-                        num_gammas, nglab, bins=self.ebins)
+        if "egtbarnu" is in vector_qs:
+            for l, nu in enumerate(self.nubins):
+                mask        = np.where(hs.getNu() == nu)
+                num_gammas  = np.sum( hs.getNug() [mask] )
+                nglab       = hs.getGamElab() [mask]
+                _ , vector_qs["egtbarnu"][n,l] = hist_from_list_of_lists(
+                            num_gammas, nglab, bins=self.ebins)
 
         # Z dependent
         for l, z in enumerate(self.zbins):
             mask  = np.where(hs.Z == z)
-            vector_qs[ "nubarZ"][n,l] = np.mean( hs.getNu()  [mask] )
-            vector_qs["nugbarZ"][n,l] = np.mean( hs.getNug() [mask] )
+            if "nubarZ" is in vector_qs:
+                vector_qs[ "nubarZ"][n,l] = np.mean( hs.getNu()  [mask] )
+            if "nugbarZ" is in vector_qs:
+                vector_qs["nugbarZ"][n,l] = np.mean( hs.getNug() [mask] )
 
         # TKE dependent
         for l in range(self.TKEbins.size):
@@ -191,28 +224,33 @@ class HistData:
             TKE_max = self.TKEbins[l+1]
             TKE     = hs.getTKEpost()
             mask    = np.logical_and( TKE >= TKE_min , TKE < TKE_max)
-            num_ns  = np.sum( hs.getNu()  [mask] )
-            num_gs  = np.sum( hs.getNug() [mask] )
+
+            num_neutrons  = np.sum( hs.getNu()  [mask] )
+            num_gammas    = np.sum( hs.getNug() [mask] )
 
             # < nu | TKE >
-            vector_qs[ "nubarTKE"][n,l] = np.mean( hs.getNutot()[mask] )
-            vector_qs["nugbarTKE"][n,l] = np.mean( hs.getNugtot()[mask] )
+            if "nubarTKE" is in vector_qs:
+                vector_qs[ "nubarTKE"][n,l] = np.mean( hs.getNutot()[mask] )
+            if "nugbarTKE" is in vector_qs:
+                vector_qs["nugbarTKE"][n,l] = np.mean( hs.getNugtot()[mask] )
 
             # < nu | E_n, TKE >
-            nelab  = hs.getNeutronElab()[mask]
-            necm   = hs.getNeutronEcm()[mask]
-            KE_pre = hs.getKEpre()[mask] / hs.getA()[mask]
+            if "pfnsTKE" is in tensor_qs:
+                nelab  = hs.getNeutronElab()[mask]
+                necm   = hs.getNeutronEcm()[mask]
+                KE_pre = hs.getKEpre()[mask] / hs.getA()[mask]
 
-            def kinematic_cut(hist : int):
-                return np.where( np.array(necm[hist]) > KE_pre[hist] / float(a) )
+                def kinematic_cut(hist : int):
+                    return np.where( np.array(necm[hist]) > KE_pre[hist] / float(a) )
 
-            tensor_qs["pfnsTKE"][n,l,:], _ = hist_from_list_of_lists(
-                    num_neutrons, nelab, bins=self.ebins, mask_generator=kinematic_cut)
+                tensor_qs["pfnsTKE"][n,l,:], _ = hist_from_list_of_lists(
+                        num_neutrons, nelab, bins=self.ebins, mask_generator=kinematic_cut)
 
             # < nu_g | E_g, TKE >
-            nglab      = hs.getGammaElab()[mask]
-            tensor_qs["pfgsTKE"][n,l,:], _ = hist_from_list_of_lists(
-                        num_gammas, nglab, bins=self.ebins)
+            if "pfgsTKE" is in tensor_qs:
+                nglab      = hs.getGammaElab()[mask]
+                tensor_qs["pfgsTKE"][n,l,:], _ = hist_from_list_of_lists(
+                            num_gammas, nglab, bins=self.ebins)
 
         # A dependent
         for l, a in enumerate(self.abins):
@@ -221,40 +259,44 @@ class HistData:
             num_gs = np.sum( hs.getNug() [mask] )
 
             # < * | A >
-            vector_qs[ "nubarA"][n,l] = np.mean( hs.nubar()   [mask] )
-            vector_qs["nugbarA"][n,l] = np.mean( hs.nugbarg() [mask] )
+            if "nubarA" is in vector_qs:
+                vector_qs[ "nubarA"][n,l] = np.mean( hs.nubar()   [mask] )
+            if "nugbarA" is in vector_qs:
+                vector_qs["nugbarA"][n,l] = np.mean( hs.nugbarg() [mask] )
 
-            mult_ratio =  hs.getNug() [mask] / hs.gteNu() [mask]
-            vector_qs["multratioA"] = np.mean( mult_ratio )
+            if "multratioA" is in vector_qs:
+                mult_ratio =  hs.getNug() [mask] / hs.gteNu() [mask]
+                vector_qs["multratioA"] = np.mean( mult_ratio )
 
             # < nu | E_n, A >
-            nelab  = hs.getNeutronElab()[mask]
-            necm   = hs.getNeutronEcm()[mask]
-            KE_pre = hs.getKEpre()[mask] / float(a)
+            if "pfnsA" is in tensor_qs:
+                nelab  = hs.getNeutronElab()[mask]
+                necm   = hs.getNeutronEcm()[mask]
+                KE_pre = hs.getKEpre()[mask] / float(a)
 
-            def kinematic_cut(hist : int):
-                return np.where( np.array(necm[hist]) > KE_pre[hist] / float(a) )
+                def kinematic_cut(hist : int):
+                    return np.where( np.array(necm[hist]) > KE_pre[hist] / float(a) )
 
-            tensor_qs["pfnsA"][n,l,:], _ = hist_from_list_of_lists(
-                    num_ns, nelab, bins=self.ebins, mask_generator=kinematic_cut)
+                tensor_qs["pfnsA"][n,l,:], _ = hist_from_list_of_lists(
+                        num_ns, nelab, bins=self.ebins, mask_generator=kinematic_cut)
 
             # < nu_g | E_g, A >
-            nglab      = hs.getGammaElab()[mask]
-            tensor_qs["pfgsA"][n,l,:], _ = hist_from_list_of_lists(
-                        num_gs, nglab, bins=self.ebins)
+            if "pfgsA" is in tensor_qs:
+                nglab      = hs.getGammaElab()[mask]
+                tensor_qs["pfgsA"][n,l,:], _ = hist_from_list_of_lists(
+                            num_gs, nglab, bins=self.ebins)
 
             # < nu | TKE, A >
-            for m in range(self.TKEbins.size):
-                TKE_min = self.TKEbins[l]
-                TKE_max = self.TKEbins[l+1]
-                TKE     = hs.getTKEpost()
-                mask    = np.logical_and(
-                            np.logical_and( TKE >= TKE_min , TKE < TKE_max),
-                            np.logical_or( hs.getAHF() == a , hs.getALF() == a )
-                        )
-                tensor_qs["nuTKEA"][n,l,m] = np.mean( hs.getNutot()[mask] )
-
-
+            if "nuATKE" is in tensor_qs:
+                for m in range(self.TKEbins.size):
+                    TKE_min = self.TKEbins[l]
+                    TKE_max = self.TKEbins[l+1]
+                    TKE     = hs.getTKEpost()
+                    mask    = np.logical_and(
+                                np.logical_and( TKE >= TKE_min , TKE < TKE_max),
+                                np.logical_or( hs.getAHF() == a , hs.getALF() == a )
+                            )
+                    tensor_qs["nuATKE"][n,l,m] = np.mean( hs.getNutot()[mask] )
 
     def post_process():
         print("Running ensembles {} to {}".format(self.min_ensemble, self.max_ensemble))
