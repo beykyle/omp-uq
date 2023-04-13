de24e28047fdf8e90dfd6c65d793c11ec690e7b7
Kyle Andrew Beyer <beykyle@umich.edu>
2023-04-12 22:37:03 -0400

    formatting

--- examples/post_proc/run_post_proc.sh
+++ examples/post_proc/run_post_proc.sh
@@ -16,9 +16,9 @@
 # each rank being bound to 1 of the node's 2 sockets
 mpiexec -bind-to socket:2 python3 -m run_post_proc 0 299
 
-# NOTE
-# because this is memory limited (loading and processing few GB from disk per file)
-# we want to bind to units that do no share memory. In the case of Armis2, we have Intel(R) Xeon(R) CPU E5-2680,
-# with 2 sockets per node, and 12 cores per socket, for a total of 24 cores. However, the 12 cores on the socket
-# share L3 cache and main memory, so binding to cores will cause each core to stomp on eachother's cache and memory,
-# and dramatically slow down, hence the -bind-to socket:2 and --ntasks-per-socket=1.
+# NOTE because this is memory limited (loading and processing few GB from disk per file)
+# we want to bind to units that do no share memory. In the case of Armis2, we have
+# Intel(R) Xeon(R) CPU E5-2680, with 2 sockets per node, and 12 cores per socket, for a
+# total of 24 cores. However, the 12 cores on the socket share L3 cache and main memory,
+# so binding to cores will cause each core to stomp on eachother's cache and memory, and
+# dramatically slow down, hence the -bind-to socket:2 and --ntasks-per-socket=1.
