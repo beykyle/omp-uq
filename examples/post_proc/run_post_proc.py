a5f5289b19a9418d36bc3a8b4c981fb1d937bd22
Kyle Andrew Beyer <beykyle@umich.edu>
2023-04-12 21:47:29 -0400

    update post processing driver scripts to correctly handle MPI gathering

--- examples/post_proc/run_post_proc.py
+++ examples/post_proc/run_post_proc.py
@@ -1,15 +1,16 @@
 from mpi4py import MPI
-from omp_uq import HistData, all_quantities
 import sys
+
+from omp_uq import HistData
 
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    data = HistData((int(sys.argv[1]), int(sys.argv[2])), all_quantities)
-    data.post_process(mpi_comm=comm)
-    data.gather(comm, rank, size)
+    data = HistData((int(sys.argv[1]), int(sys.argv[2])), ["nubar"])
+    rank_slice = data.post_process(mpi_comm=comm)
+    data.gather(comm, rank, size, rank_slice)
     if rank == 0:
         print('Writing output to "{}/*.npy"'.format(str(data.res_dir)))
         sys.stdout.flush()
--- examples/post_proc/run_post_proc.sh
+++ examples/post_proc/run_post_proc.sh
@@ -2,8 +2,9 @@
 
 #SBATCH --job-name mppchcf
 #SBATCH --nodes=10
-#SBATCH --mem-per-cpu=12GB
+#SBATCH --mem=48GB
 #SBATCH --sockets-per-node=2
+#SBATCH --ntasks-per-socket=1
 #SBATCH --time=2:00:00
 #SBATCH --account=bckiedro98
 #SBATCH --partition=standard
@@ -11,13 +12,13 @@
 #SBATCH -e slurm-%j.err
 #SBATCH -o slurm-%j.out
 
-# Process CGMF history files in parallell, with 2 ranks per node,
-# assigned to both node's sockets
+# Process CGMF history files in parallell, with 2 MPI ranks per node,
+# each rank being bound to 1 of the node's 2 sockets
 mpiexec -bind-to socket:2 python3 -m run_post_proc 0 299
 
 # NOTE
 # because this is memory limited (loading and processing few GB from disk per file)
 # we want to bind to units that do no share memory. In the case of Armis2, we have Intel(R) Xeon(R) CPU E5-2680,
-# with 2 sockets per node, and 12 cores per socket, for a total of 23 cores. However, the 12 cores on the socket
+# with 2 sockets per node, and 12 cores per socket, for a total of 24 cores. However, the 12 cores on the socket
 # share L3 cache and main memory, so binding to cores will cause each core to stomp on eachother's cache and memory,
-# and dramatically slow down, hence the -bind-to socket:2.
+# and dramatically slow down, hence the -bind-to socket:2 and --ntasks-per-socket=1.
