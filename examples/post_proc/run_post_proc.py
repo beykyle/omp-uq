c1cedb1a01d15faad50d2219f63e57341ffb4fa5
Kyle Andrew Beyer <beykyle@umich.edu>
2023-04-15 18:29:12 -0400

    specify directories, use all q's

--- examples/post_proc/run_post_proc.py
+++ examples/post_proc/run_post_proc.py
@@ -1,14 +1,19 @@
-from mpi4py import MPI
 import sys
+from mpi4py import MPI
 
-from omp_uq import HistData
+from cgmf_analysis import HistData, all_quantities
 
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    data = HistData((int(sys.argv[1]), int(sys.argv[2])), ["nubar"])
+    data = HistData(
+        (int(sys.argv[1]), int(sys.argv[2])),
+        quantities=all_quantities,
+        hist_dir="./",
+        res_dir="./chuq_cf_cgmf/",
+    )
     rank_slice = data.post_process(mpi_comm=comm)
     data.gather(comm, rank, size, rank_slice)
     if rank == 0:
