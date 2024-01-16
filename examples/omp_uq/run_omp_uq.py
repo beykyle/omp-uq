34090129c361af75c734ed25a84293c3cf5d3618
beykyle <beykyle@umich.edu>
2024-01-16 14:06:04 -0500

    update examples

--- examples/omp_uq/run_omp_uq.py
+++ examples/omp_uq/run_omp_uq.py
@@ -1,7 +1,7 @@
 # instructions to run:
 #   mpirun -n {nproc} python run_comp_uq.py
 
-from pyCGMF import CGMF_Input, run
+from pyCGMF import Input, run
 from CGMFtk.histories import Histories
 
 import sys
@@ -10,7 +10,10 @@ from mpi4py import MPI
 from pathlib import Path
 
 
-def run_cgmf_mpi(inp: CGMF_Input, comm, name: str):
+def run_cgmf_mpi(inp: Input, comm, name: str, output_dir: Path):
+    print("Running {} histories on rank {}".format(inp.nevents, inp.MPI_rank))
+    sys.stdout.flush()
+
     # run worker on each rank
     hists = run(inp)
 
@@ -22,53 +25,78 @@ def run_cgmf_mpi(inp: CGMF_Input, comm, name: str):
         # create new Histories object with history info from all MPI ranks,
         # write the output to disk as binary np array
         all_histories = Histories(from_arr=np.concatenate(result, axis=0))
-        out_fname = Path("./histories_{}.npy".format(name))
+        out_fname = Path(output_dir / "histories_{}.npy".format(name))
         all_histories.save(out_fname)
-        print("Writing sample output to {}".format(out_fname))
+        print("Writing output to {}".format(out_fname))
+        sys.stdout.flush()
         return all_histories
 
     return None
 
 
-def main():
+def run_samples(
+    start,
+    end,
+    sample_omp_dir,
+    output_dir,
+    omp_prefix,
+    zaid=98252,
+    einc=0.0,
+    histories_per_sample=1e6,
+):
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
 
-    histories_per_sample = 2400
+    histories_per_sample = int(histories_per_sample)
     histories_per_MPI_rank = histories_per_sample // size
 
-    sample_omp_path = Path("/home/beykyle/omplib/data/KDUQSamples/samples/")
+    if rank == 0:
+        if not output_dir.is_dir():
+            output_dir.mkdir(parents=True)
 
-    (start, end) = (int(sys.argv[1]), int(sys.argv[2]))
-    end += 1
-
-    for s in range(start, end):
+    for s in range(start, end+1):
         name = str(s)
-        omp_sample_fname = sample_omp_path / "kd_{}.json".format(s)
+        omp_sample_fname = sample_omp_dir / f"{omp_prefix}_{s}.json"
         assert omp_sample_fname.is_file()
+        sys.stdout.flush()
         if rank == 0:
             print(
-                "Running CGMF w/ sample file {}, with {} histories on {} ranks."
-                "\nHistories per MPI rank: {}".format(
+                "Running CGMF w/ sample file {}, "
+                "with {} histories, on {} ranks.\n"
+                "Histories per MPI rank: {}".format(
                     omp_sample_fname,
                     histories_per_sample,
-                    rank,
+                    size,
                     histories_per_MPI_rank,
                 )
             )
+        sys.stdout.flush()
 
-        inp = CGMF_Input(
-            nevents=histories_per_MPI_rank,
-            zaid=98252,
-            einc=0.0,
-            omp_fpath=str(omp_sample_fname),
+        inp = Input(
+            nevents=int(histories_per_MPI_rank),
+            zaid=zaid,
+            einc=einc,
             MPI_rank=rank,
-            seed=(s + 177713957),
+            seed=3957 * s + s + 13,
+            omp_fpath=str(omp_sample_fname),
         )
 
         # run on all workers
-        run_cgmf_mpi(inp, comm, name)
+        run_cgmf_mpi(inp, comm, name, output_dir)
+
+
+def main():
+
+    run_samples(
+        start = int(sys.argv[1]),
+        end = int(sys.argv[2]),
+        sample_omp_dir = Path("/home/beykyle/omplib/data/WLHSamples/samples/"),
+        output_dir = Path("/home/beykyle/turbo/omp_uq/run3_all/cf252/wlh"),
+        omp_prefix = "wlh",
+        zaid = 98252,
+        einc = 0.0,
+    )
 
 
 if __name__ == "__main__":
--- examples/post_proc/run_post_proc.sh
+++ examples/post_proc/run_post_proc.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-#SBATCH --job-name chuq_cf_process_hist
-#SBATCH --nodes=10
-#SBATCH --mem-per-cpu=10GB
-#SBATCH --sockets-per-node=2
-#SBATCH --ntasks-per-socket=1
-#SBATCH --time=2:00:00
-#SBATCH --account=bckiedro98
+#SBATCH --job-name p3_kdcf
+#SBATCH --nodes=2
+#SBATCH --mem=180GB
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --account=bckiedro0
 #SBATCH --partition=standard
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -e slurm-%j.err
 #SBATCH -o slurm-%j.out
 
-# Process CGMF history files in parallell, with 2 MPI ranks per node,
-# each rank being bound to 1 of the node's 2 sockets
-mpirun python3 -m run_post_proc 0 299
+srun python3 run_post_proc.py 0 299 ~/turbo/omp_uq/run3_all/cf252/kduq/ ~/turbo/omp_uq/run3_all/cf252/kduq/kduq_post/
+#srun python3 run_post_proc.py 0 299 ~/turbo/omp_uq/run3_all/cf252/chuq ~/turbo/omp_uq/run3_all/cf252/chuq/chuq_post
+#srun python3 run_post_proc.py 0 299 ~/turbo/omp_uq/run3_all/cf252/wlh/ ~/turbo/omp_uq/run3_all/cf252/wlh/wlh_post/
 
-# NOTE because this is memory limited (loading and processing few GB from disk per file)
-# we want to bind to units that do no share memory. In the case of Armis2, we have
-# Intel(R) Xeon(R) CPU E5-2680, with 2 sockets per node, and 12 cores per socket, for a
-# total of 24 cores. However, the 12 cores on each socket share L3 cache and main memory,
-# so binding to cores rather than sockets will cause each core to stomp on eachother's
-# cache and memory, and dramatically slow down, hence --ntasks-per-socket=1.
+#srun python3 run_post_proc.py 0 299 ~/turbo/omp_uq/run3_all/u235/kduq/ ~/turbo/omp_uq/run3_all/u235/kduq/kduq_post/
+#srun python3 run_post_proc.py 0 299 ~/turbo/omp_uq/run3_all/u235/chuq/ ~/turbo/omp_uq/run3_all/u235/chuq/chuq_post
+#srun python3 run_post_proc.py 0 299 ~/turbo/omp_uq/run3_all/u235/wlh/ ~/turbo/omp_uq/run3_all/u235/wlh/wlh_post/
